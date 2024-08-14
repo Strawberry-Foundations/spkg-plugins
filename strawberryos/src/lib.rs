16d2672aa6956e384a735afc380dec183c62670a
Juliandev02 <julian@strawberryfoundations.xyz>
2024-08-14 23:22:35 +0200

    fix: fix errors due to libspkg update

--- strawberryos/src/lib.rs
+++ strawberryos/src/lib.rs
@@ -3,15 +3,6 @@ use libspkg::plugin::{Plugin, PluginProperties};
 pub struct ExamplePlugin;
 
 impl Plugin for ExamplePlugin {
-    fn properties(&self) -> PluginProperties {
-        PluginProperties {
-            name: "StrawberryOS",
-            id: "strawberryos",
-            package_id: "org.strawberryfoundations.spkg.plugins.strawberryos",
-            version: "0.1.0",
-        }
-    }
-
     fn execute(&self, args: &[String]) {
         match args.first().unwrap().as_str() {
             "test" => {
@@ -31,6 +22,14 @@ impl Plugin for ExamplePlugin {
 
 #[allow(improper_ctypes_definitions)]
 #[no_mangle]
-pub extern "C" fn create_plugin() -> Box<dyn Plugin> {
-    Box::new(ExamplePlugin)
+pub extern "C" fn create_plugin() -> (Box<dyn Plugin>, PluginProperties) {
+    let properties: PluginProperties = PluginProperties {
+        name: "StrawberryOS",
+        id: "strawberryos",
+        package_id: "org.strawberryfoundations.spkg.plugins.strawberryos",
+        version: env!("CARGO_PKG_VERSION"),
+        library_version: libspkg::VERSION,
+    };
+
+    (Box::new(ExamplePlugin), properties)
 }
